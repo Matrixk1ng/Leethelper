e21070fafa992a63e6989d4fdaa20cb97a7fb914
Obinna Anaenugwu <oeanaenugwu@gmail.com>
2025-04-15 15:52:10 -0500

    fixed book mark bug and moved news file to app folder

--- Cobra/app/(tabs)/_layout.tsx
+++ Cobra/app/(tabs)/_layout.tsx
@@ -1,38 +1,43 @@
 import { Tabs } from "expo-router";
 import FontAwesome from "@expo/vector-icons/FontAwesome";
+import { SavedNewsProvider } from "@/context/SavedNewsContext";
 
 export default function TabsLayout() {
   return (
-    <Tabs screenOptions={{
-      headerShown: false, // Hide header for all screens
-    }}>
-      <Tabs.Screen
-        name="home"
-        options={{
-          title: "home",
-          tabBarIcon: ({ color } : {color: string}) => (
-            <FontAwesome size={28} name="home" color={color} />
-          ),
+    <SavedNewsProvider>
+      <Tabs
+        screenOptions={{
+          headerShown: false, // Hide header for all screens
         }}
-      />
-      <Tabs.Screen
-        name="saved"
-        options={{
-          title: "saved",
-          tabBarIcon: ({ color } : {color: string}) => (
-            <FontAwesome size={28} name="bookmark" color={color} />
-          ),
-        }}
-      ></Tabs.Screen>
-      <Tabs.Screen
-        name="profile"
-        options={{
-          title: "profile",
-          tabBarIcon: ({ color } : {color: string}) => (
-            <FontAwesome size={28} name="user" color={color} />
-          ),
-        }}
-      ></Tabs.Screen>
-    </Tabs>
+      >
+        <Tabs.Screen
+          name="home"
+          options={{
+            title: "home",
+            tabBarIcon: ({ color }: { color: string }) => (
+              <FontAwesome size={28} name="home" color={color} />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="saved"
+          options={{
+            title: "saved",
+            tabBarIcon: ({ color }: { color: string }) => (
+              <FontAwesome size={28} name="bookmark" color={color} />
+            ),
+          }}
+        ></Tabs.Screen>
+        <Tabs.Screen
+          name="profile"
+          options={{
+            title: "profile",
+            tabBarIcon: ({ color }: { color: string }) => (
+              <FontAwesome size={28} name="user" color={color} />
+            ),
+          }}
+        ></Tabs.Screen>
+      </Tabs>
+    </SavedNewsProvider>
   );
 }
